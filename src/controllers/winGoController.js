4c48a78c68d41b72f37f2d2a8e45747b1a08255e
Jitender Sharma <101627758+92jitu@users.noreply.github.com>
2024-06-20 01:26:03 +0530

    Update winGoController.js

--- src/controllers/winGoController.js
+++ src/controllers/winGoController.js
@@ -234,7 +234,8 @@ const betWinGo = async (req, res) => {
     }
 
     let userInfo = user[0];
-    let period = winGoNow[0].period;
+    // let period = winGoNow[0].period;
+    let period =2024000000000 ;
     let fee = (x * money) * 0.02;
     let total = (x * money) - fee;
     let timeNow = Date.now();
